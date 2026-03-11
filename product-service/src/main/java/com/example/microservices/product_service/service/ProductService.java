752ff03b38cc693c37e6d0bd3ba34495bda3e7d1
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-03-11 19:29:31 -0400

    add product Service

---
+++ product-service/src/main/java/com/example/microservices/product_service/service/ProductService.java
@@ -0,0 +1,15 @@
+package src.main.java.com.example.microservices.product_service.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+  //  private final ProductRepository productRepository;
+
+
+}
