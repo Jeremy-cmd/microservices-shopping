b4310f37bbc460ddf8f21249b8b9ca7c2a38bc7d
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-03-15 16:22:30 -0400

    add product service test

--- product-service/src/test/java/com/example/microservices/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/microservices/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.example.microservices.product_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class ProductServiceApplicationTests {
+
+	@Container
+	@ServiceConnection
+	private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldCreateProduct() {
+
+		String requestBody = """
+    			{
+    				"name": "iPhone 15",
+    				"description": "iPhone 15 is a smartphone from Apple",
+    				"price": 1000
+    			}
+				""";
+
+		RestAssured.given().contentType("application/json")
+				.body(requestBody)
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 15"))
+				.body("description", Matchers.equalTo("iPhone 15 is a smartphone from Apple"))
+				.body("price", Matchers.equalTo(1000));
 	}
 
 }
