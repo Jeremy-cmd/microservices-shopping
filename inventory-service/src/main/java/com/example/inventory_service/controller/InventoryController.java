ba4cbe834a1495ae08202144a1514595ade7e69a
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-03-19 19:33:03 -0400

    uncomment

--- inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
@@ -1,5 +1,6 @@
 package com.example.inventory_service.controller;
 
+import com.example.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class InventoryController {
 
-    // private final InventoryService inventoryService;
+     private final InventoryService inventoryService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
-        // return inventoryService.isInStock(skuCode, quantity);
-        return false;
+         return inventoryService.isInStock(skuCode, quantity);
     }
 }
