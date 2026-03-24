8dd1ff944435549dac3a9a0a0d3a5c8128a4a339
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-03-24 17:18:56 -0400

    set observation for kafka template

--- order-service/src/main/java/com/example/order_service/config/ObservationConfig.java
+++ order-service/src/main/java/com/example/order_service/config/ObservationConfig.java
@@ -6,10 +6,18 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 @RequiredArgsConstructor
 public class ObservationConfig {
+
+    private final KafkaTemplate kafkaTemplate;
+
+    @PostConstruct
+    public void setObservationForKafkaTemplate() {
+        kafkaTemplate.setObservationEnabled(true);
+    }
 
     @Bean
     ObservedAspect observedAspect(ObservationRegistry registry) {
